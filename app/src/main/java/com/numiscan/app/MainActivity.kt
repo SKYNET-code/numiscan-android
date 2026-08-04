674677304474eeb3078b185801f36c7ce3fd850a
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-05 02:17:58 +0330

    Update MainActivity.kt

--- app/src/main/java/com/numiscan/app/MainActivity.kt
+++ app/src/main/java/com/numiscan/app/MainActivity.kt
@@ -84,13 +84,11 @@ class MainActivity : ComponentActivity() {
     }
 
 
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
 
         super.onNewIntent(intent)
 
-        intent?.let {
-
-            handleShareIntent(it)
+        handleShareIntent(it)
 
         }
 
