56b6162da18f8aba18f2e5ab5675f7114a7c0086
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 13:38:04 +0330

    Update ClipboardManager.kt

--- app/src/main/java/com/numiscan/app/utils/ClipboardManager.kt
+++ app/src/main/java/com/numiscan/app/utils/ClipboardManager.kt
@@ -1,5 +1,6 @@
 package com.numiscan.app.utils
 
+
 import android.content.ClipData
 import android.content.Context
 
@@ -13,7 +14,7 @@ object ClipboardManager {
 
         text: String
 
-    ) {
+    ){
 
 
         val clipboard =
@@ -27,11 +28,50 @@ object ClipboardManager {
         clipboard.setPrimaryClip(
 
             ClipData.newPlainText(
+
                 "NumiScan",
+
                 text
+
             )
 
         )
+
+    }
+
+
+
+    fun paste(
+
+        context: Context
+
+    ): String {
+
+
+        val clipboard =
+            context.getSystemService(
+                Context.CLIPBOARD_SERVICE
+            )
+            as android.content.ClipboardManager
+
+
+
+        if(
+            !clipboard.hasPrimaryClip()
+        ){
+
+            return ""
+
+        }
+
+
+
+        return clipboard
+            .primaryClip
+            ?.getItemAt(0)
+            ?.text
+            ?.toString()
+            ?: ""
 
     }
 
