471f82370962f6119a9ce35b0303f12ec8f75107
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 13:26:56 +0330

    Create ExtractedNumber.kt

---
+++ app/src/main/java/com/numiscan/app/data/model/ExtractedNumber.kt
@@ -0,0 +1,13 @@
+package com.numiscan.app.data.model
+
+data class ExtractedNumber(
+
+    val value: String,
+
+    val type: NumberType,
+
+    val sourceText: String = "",
+
+    val selected: Boolean = false
+
+)
