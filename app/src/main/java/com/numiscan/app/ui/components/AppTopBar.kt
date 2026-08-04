b0897d1cfec12db02a50e795aa7a65af1d417c38
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 21:10:22 +0330

    Update AppTopBar.kt

--- app/src/main/java/com/numiscan/app/ui/components/AppTopBar.kt
+++ app/src/main/java/com/numiscan/app/ui/components/AppTopBar.kt
@@ -1,12 +1,21 @@
 package com.numiscan.app.ui.components
 
-
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.*
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.numiscan.app.ui.theme.Background
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -16,34 +25,45 @@ fun AppTopBar(
 
     onMenuClick: () -> Unit
 
-){
+) {
 
+    CenterAlignedTopAppBar(
 
-    TopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
 
-        title = {
+            containerColor = Background
 
-            Text(title)
-
-        },
-
+        ),
 
         navigationIcon = {
-
 
             IconButton(
 
                 onClick = onMenuClick
 
-            ){
-
+            ) {
 
                 Icon(
 
-                    imageVector =
-                        Icons.Default.Menu,
+                    imageVector = Icons.Rounded.Menu,
 
                     contentDescription = null
+
+                )
+
+            }
+
+        },
+
+        title = {
+
+            Row {
+
+                Text(
+
+                    text = title,
+
+                    style = MaterialTheme.typography.titleLarge
 
                 )
 
