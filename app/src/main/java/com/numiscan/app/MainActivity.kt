593ec635f49bed22cdfc41b37a000f797b834918
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 21:18:47 +0330

    Update MainActivity.kt

--- app/src/main/java/com/numiscan/app/MainActivity.kt
+++ app/src/main/java/com/numiscan/app/MainActivity.kt
@@ -1,88 +1,31 @@
 package com.numiscan.app
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
-import com.numiscan.app.ui.screens.HomeScreen
-import com.numiscan.app.ui.screens.ResultsScreen
+import com.numiscan.app.navigation.AppNavigation
 import com.numiscan.app.ui.theme.NumiScanTheme
 
+class MainActivity:ComponentActivity(){
 
+    override fun onCreate(
 
-class MainActivity : ComponentActivity() {
+        savedInstanceState:Bundle?
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    ){
 
         super.onCreate(savedInstanceState)
 
+        setContent{
 
-        setContent {
+            NumiScanTheme{
 
-
-            NumiScanTheme {
-
-
-                var currentScreen by remember {
-
-                    mutableStateOf("home")
-
-                }
-
-
-
-                when(currentScreen){
-
-
-                    "home" -> {
-
-
-                        HomeScreen(
-
-                            onOpenResults = {
-
-                                currentScreen = "results"
-
-                            }
-
-                        )
-
-
-                    }
-
-
-
-                    "results" -> {
-
-
-                        ResultsScreen(
-
-                            onBack = {
-
-                                currentScreen = "home"
-
-                            }
-
-                        )
-
-
-                    }
-
-
-                }
-
+                AppNavigation()
 
             }
 
-
         }
 
-
     }
-
 
 }
