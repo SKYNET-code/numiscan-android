15fc94a2eea04aacbde45951483cfdb6655906c8
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 21:23:22 +0330

    Update NumberIcon.kt

--- app/src/main/java/com/numiscan/app/ui/components/NumberIcon.kt
+++ app/src/main/java/com/numiscan/app/ui/components/NumberIcon.kt
@@ -3,47 +3,32 @@ package com.numiscan.app.ui.components
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.CreditCard
-import androidx.compose.material.icons.outlined.Email
-import androidx.compose.material.icons.outlined.Language
 import androidx.compose.material.icons.outlined.PhoneAndroid
 import androidx.compose.material.icons.outlined.AccountBalance
-import androidx.compose.material.icons.outlined.HelpOutline
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.numiscan.app.data.model.NumberType
 
 @Composable
 fun NumberIcon(
+    type: NumberType
+) {
 
-    type:NumberType
-
-){
-
-    val icon:ImageVector=
-
-        when(type){
+    val icon =
+        when (type) {
 
             NumberType.MOBILE ->
                 Icons.Outlined.PhoneAndroid
 
-            NumberType.PHONE ->
+            NumberType.LANDLINE ->
                 Icons.Outlined.Call
 
-            NumberType.CARD ->
+            NumberType.BANK_CARD ->
                 Icons.Outlined.CreditCard
 
             NumberType.SHABA ->
                 Icons.Outlined.AccountBalance
-
-            NumberType.EMAIL ->
-                Icons.Outlined.Email
-
-            NumberType.URL ->
-                Icons.Outlined.Language
-
-            NumberType.UNKNOWN ->
-                Icons.Outlined.HelpOutline
 
         }
 
