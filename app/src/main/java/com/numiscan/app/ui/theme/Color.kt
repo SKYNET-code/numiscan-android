39d3b2f57eb738a7d42d46cf06cc3afd4e62fe37
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 13:22:44 +0330

    Create Color.kt

---
+++ app/src/main/java/com/numiscan/app/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.numiscan.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val PrimaryBlue = Color(0xFF1976D2)
+
+val SecondaryTeal = Color(0xFF00A7A7)
+
+val BackgroundLight = Color(0xFFF8F9FA)
+
+val BackgroundDark = Color(0xFF121212)
