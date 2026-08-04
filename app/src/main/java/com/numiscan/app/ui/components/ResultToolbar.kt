2466f43923447a44096d690c04aec193a1ff7e45
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 14:47:07 +0330

    Update ResultToolbar.kt

--- app/src/main/java/com/numiscan/app/ui/components/ResultToolbar.kt
+++ app/src/main/java/com/numiscan/app/ui/components/ResultToolbar.kt
@@ -1,33 +1,40 @@
 package com.numiscan.app.ui.components
 
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 
 
 @Composable
-fun ResultToolbar(){
+fun ResultToolbar(
+
+    onSelectAll: () -> Unit,
+
+    onClear: () -> Unit
+
+) {
 
 
     Row(
 
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
+        modifier = Modifier.fillMaxWidth(),
 
         horizontalArrangement =
             Arrangement.spacedBy(8.dp)
 
-    ){
+    ) {
 
 
         Button(
 
-            onClick = {}
+            onClick = onSelectAll
 
         ){
 
@@ -39,7 +46,7 @@ fun ResultToolbar(){
 
         Button(
 
-            onClick = {}
+            onClick = onClear
 
         ){
 
