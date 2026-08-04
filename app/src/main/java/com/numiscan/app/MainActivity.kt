4453b244648420fddacaf156515ed3902e0b6d3a
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-05 00:29:55 +0330

    Update MainActivity.kt

--- app/src/main/java/com/numiscan/app/MainActivity.kt
+++ app/src/main/java/com/numiscan/app/MainActivity.kt
@@ -4,23 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.numiscan.app.navigation.AppNavigation
+import com.numiscan.app.ui.screens.HomeScreen
 import com.numiscan.app.ui.theme.NumiScanTheme
+import com.numiscan.app.viewmodel.MainViewModel
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.collectAsState
+
 
 class MainActivity : ComponentActivity() {
 
-    override fun onCreate(
 
-        savedInstanceState: Bundle?
+    private val viewModel: MainViewModel by viewModels()
 
-    ) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
+
         setContent {
+
 
             NumiScanTheme {
 
-                AppNavigation()
+
+                val state by viewModel.uiState.collectAsState()
+
+
+                AppNavigation {
+
+
+                    HomeScreen(
+
+                        state = state,
+
+                        onTextChange = viewModel::updateText,
+
+                        onSearchChange = viewModel::updateSearch,
+
+                        onFilterChange = viewModel::setFilter,
+
+                        onMenuClick = {}
+
+                    )
+
+                }
+
 
             }
 
