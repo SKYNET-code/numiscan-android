cac77c48cb9c84332faf708da0f3b33f669eeb1f
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-05 00:27:03 +0330

    Update Typography.kt

--- app/src/main/java/com/numiscan/app/ui/theme/Typography.kt
+++ app/src/main/java/com/numiscan/app/ui/theme/Typography.kt
@@ -1,42 +1,5 @@
 package com.numiscan.app.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
-val AppTypography = Typography(
-
-    headlineLarge = TextStyle(
-        fontSize = 30.sp,
-        fontWeight = FontWeight.Bold
-    ),
-
-    headlineMedium = TextStyle(
-        fontSize = 24.sp,
-        fontWeight = FontWeight.Bold
-    ),
-
-    titleLarge = TextStyle(
-        fontSize = 20.sp,
-        fontWeight = FontWeight.Bold
-    ),
-
-    titleMedium = TextStyle(
-        fontSize = 17.sp,
-        fontWeight = FontWeight.SemiBold
-    ),
-
-    bodyLarge = TextStyle(
-        fontSize = 16.sp
-    ),
-
-    bodyMedium = TextStyle(
-        fontSize = 14.sp
-    ),
-
-    labelLarge = TextStyle(
-        fontSize = 15.sp,
-        fontWeight = FontWeight.Medium
-    )
-)
+val AppTypography = Typography()
