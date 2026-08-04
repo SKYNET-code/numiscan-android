67cad10f154e61ccffe2f71d0a3e965c1420d47d
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-05 00:26:26 +0330

    Update Shape.kt

--- app/src/main/java/com/numiscan/app/ui/theme/Shape.kt
+++ app/src/main/java/com/numiscan/app/ui/theme/Shape.kt
@@ -4,15 +4,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes(
+val AppShapes = Shapes(
 
-    extraSmall = RoundedCornerShape(10.dp),
+    extraSmall = RoundedCornerShape(8.dp),
 
-    small = RoundedCornerShape(14.dp),
+    small = RoundedCornerShape(12.dp),
 
-    medium = RoundedCornerShape(18.dp),
+    medium = RoundedCornerShape(16.dp),
 
-    large = RoundedCornerShape(24.dp),
+    large = RoundedCornerShape(20.dp),
 
     extraLarge = RoundedCornerShape(28.dp)
 
