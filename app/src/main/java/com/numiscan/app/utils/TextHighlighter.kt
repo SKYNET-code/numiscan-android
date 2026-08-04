3f04919e7eadcf272aef89ddaaa3959f8695be14
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 13:41:33 +0330

    Update TextHighlighter.kt

--- app/src/main/java/com/numiscan/app/utils/TextHighlighter.kt
+++ app/src/main/java/com/numiscan/app/utils/TextHighlighter.kt
@@ -1,10 +1,15 @@
 package com.numiscan.app.utils
 
+
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.graphics.Color
+
 
 
 object TextHighlighter {
+
 
 
     fun highlight(
@@ -16,27 +21,59 @@ object TextHighlighter {
     ): AnnotatedString {
 
 
+
         return buildAnnotatedString {
+
 
 
             append(text)
 
 
+
             numbers.forEach { number ->
 
 
-                addStringAnnotation(
 
-                    tag = "NUMBER",
+                var start =
+                    text.indexOf(number)
 
-                    annotation = number,
 
-                    start = text.indexOf(number),
 
-                    end = text.indexOf(number)
-                        + number.length
+                while(start >= 0){
 
-                )
+
+
+                    addStyle(
+
+                        style =
+                            SpanStyle(
+
+                                color =
+                                    Color.Blue
+
+                            ),
+
+
+                        start = start,
+
+
+                        end =
+                            start + number.length
+
+                    )
+
+
+
+                    start =
+                        text.indexOf(
+
+                            number,
+
+                            start + 1
+
+                        )
+
+                }
 
             }
 
