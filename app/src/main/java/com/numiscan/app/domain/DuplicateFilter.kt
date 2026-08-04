5f8397553126ab3ef8c3e4a74445625d7e387584
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 13:27:56 +0330

    Create DuplicateFilter.kt

---
+++ app/src/main/java/com/numiscan/app/domain/DuplicateFilter.kt
@@ -0,0 +1,24 @@
+package com.numiscan.app.domain
+
+import com.numiscan.app.data.model.ExtractedNumber
+
+
+class DuplicateFilter {
+
+
+    fun removeDuplicates(
+
+        items: List<ExtractedNumber>
+
+    ): List<ExtractedNumber> {
+
+
+        return items.distinctBy {
+
+            it.value
+
+        }
+
+    }
+
+}
