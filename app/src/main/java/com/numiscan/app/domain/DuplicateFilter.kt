ab04e6ef7e90eb2a05f94469cfd869c77b80dac0
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-04 16:41:17 +0330

    Update DuplicateFilter.kt

--- app/src/main/java/com/numiscan/app/domain/DuplicateFilter.kt
+++ app/src/main/java/com/numiscan/app/domain/DuplicateFilter.kt
@@ -2,22 +2,13 @@ package com.numiscan.app.domain
 
 import com.numiscan.app.data.model.ExtractedNumber
 
-
 class DuplicateFilter {
 
-
     fun removeDuplicates(
-
-        items: List<ExtractedNumber>
-
+        list: List<ExtractedNumber>
     ): List<ExtractedNumber> {
 
-
-        return items.distinctBy {
-
-            it.value
-
-        }
+        return list.distinctBy { it.value }
 
     }
 
