a123d285807cb07261f93c15ef98d28ff9d30a57
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-06 12:48:27 +0330

    Update Color.kt

--- app/src/main/java/com/numiscan/app/ui/theme/Color.kt
+++ app/src/main/java/com/numiscan/app/ui/theme/Color.kt
@@ -2,14 +2,28 @@ package com.numiscan.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BluePrimary = Color(0xFF1565C0)
-val BlueSecondary = Color(0xFF42A5F5)
+/* ---------- Light Theme ---------- */
 
-val LightBackground = Color(0xFFF5F7FB)
+val BluePrimary = Color(0xFF2563EB)
+val BlueSecondary = Color(0xFF3B82F6)
+
+val LightBackground = Color(0xFFF4F7FB)
 val LightSurface = Color(0xFFFFFFFF)
 
-val DarkBackground = Color(0xFF101418)
-val DarkSurface = Color(0xFF1A1F24)
+val LightOutline = Color(0xFFCBD5E1)
+val LightSurfaceVariant = Color(0xFFE8EEF6)
 
-val TextPrimary = Color(0xFF1A1C1E)
-val TextSecondary = Color(0xFF5F6368)
+val LightSuccess = Color(0xFF43A047)
+val LightError = Color(0xFFDC2626)
+
+
+/* ---------- Dark Theme ---------- */
+
+val DarkBackground = Color(0xFF0F172A)
+val DarkSurface = Color(0xFF1E293B)
+
+val DarkOutline = Color(0xFF334155)
+val DarkSurfaceVariant = Color(0xFF273449)
+
+val DarkSuccess = Color(0xFF66BB6A)
+val DarkError = Color(0xFFEF4444)
