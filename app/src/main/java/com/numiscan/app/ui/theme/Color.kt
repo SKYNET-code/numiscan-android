ce88462c787b01e467ddf24b05a4b50b2c541d35
SKYNET-code <56219850+SKYNET-code@users.noreply.github.com>
2026-08-06 14:26:24 +0330

    Update Color.kt

--- app/src/main/java/com/numiscan/app/ui/theme/Color.kt
+++ app/src/main/java/com/numiscan/app/ui/theme/Color.kt
@@ -19,16 +19,16 @@ val LightError = Color(0xFFE57373)
 
 
 
-/* ---------- Dark Theme (Idea 5) ---------- */
+/* ---------- Light Theme (Idea 5) ---------- */
 
-val DarkBackground = Color(0xFF101827)
-val DarkSurface = Color(0xFF182233)
+val BluePrimary = Color(0xFF1976D2)
+val BlueSecondary = Color(0xFF00ACC1)
 
-val DarkOutline = Color(0xFF2D415A)
-val DarkSurfaceVariant = Color(0xFF223044)
+val LightBackground = Color(0xFFF6F9FC)
+val LightSurface = Color(0xFFFBFDFF)
 
-val DarkPrimary = Color(0xFF42A5F5)
-val DarkSecondary = Color(0xFF26C6DA)
+val LightOutline = Color(0xFFD7E6F3)
+val LightSurfaceVariant = Color(0xFFEAF2F8)
 
-val DarkSuccess = Color(0xFF66BB6A)
-val DarkError = Color(0xFFEF9A9A)
+val LightSuccess = Color(0xFF43A047)
+val LightError = Color(0xFFE57373)
